a8c9bdcb8b29a86eeb8ef7d6f2a3510bc98e24a3
Yazan Amer <yazan.amer@progressoft.com>
2024-01-31 12:16:50 +0300

    feat: test_app.py

--- backend/test/test_app.py
+++ backend/test/test_app.py
@@ -1,4 +1,7 @@
+import json
 import unittest
+
+from flask import Response
 
 from pathological.app import app
 
@@ -10,10 +13,38 @@ class AppTest(unittest.TestCase):
 
     def test_health(self):
         response = self.app.get("/actuator/health")
-        self.assertEqual(200, response.status_code)
+        self.assertOkay(response)
         self.assertEqual(response.json["status"], "UP")
 
-    def test_game(self):
+    def test_scenario(self):
         session_id_response = self.app.post("/api/v1/new-session-id")
-        self.assertEqual(200, session_id_response.status_code)
-        self.assertIsNotNone(session_id_response.text)
+        self.assertOkay(session_id_response)
+        self.assertGreater(len(session_id_response.text), 10)
+
+        challenge_response = self.post_with_body("api/v1/request-challenge",
+                                                 {"player_id": session_id_response.text})
+        self.assertOkay(challenge_response)
+        self.assertTrue("challenge_id" in challenge_response.json)
+
+        solution_response = self.post_with_body("/api/v1/solve-challenge",
+                                                {
+                                                    "player_id": session_id_response.text,
+                                                    "challenge_key": challenge_response.json["challenge_id"],
+                                                    "answer": "ANY"})
+        self.assertOkay(solution_response)
+
+        image_response = self.app.get(f"/api/v1/image/{challenge_response.json['image_id']}")
+        self.assertOkay(image_response)
+        self.assertEqual(image_response.content_type, "image/png")
+
+        score = self.app.get(f"/api/v1/score/{session_id_response.text}")
+        self.assertOkay(score)
+        self.assertTrue("player_score" in score.json)
+        self.assertTrue("player_id" in score.json)
+
+    def post_with_body(self, endpoint: str, body: dict) -> Response:
+        return self.app.post(endpoint, data=json.dumps(body),
+                             content_type="application/json")
+
+    def assertOkay(self, response: Response):
+        self.assertEqual(200, response.status_code)
